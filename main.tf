d299ee229b5049da66000a9104b1312ce4833909
danielperaltamadriz <danielperaltamadriz@gmail.com>
2023-04-29 20:37:32 +0200

    chapter 3:
    Related to: https://github.com/sudomateo/terraform-training/tree/main/course/03-Terraform-Language/exercises

--- main.tf
+++ main.tf
@@ -8,78 +8,60 @@ terraform {
 }
 
 provider "aws" {
-    profile = "terraform"
-    region = "eu-central-1"
+  profile = "terraform"
+  region  = "eu-central-1"
 }
 
-data "http" "my_ip" {
-  url = "https://api.ipify.org"
+provider "aws" {
+  profile = "terraform"
+  alias   = "secondary"
+  region  = "eu-west-3"
 }
 
-locals {
-    my_ip = format("%s/%s",data.http.my_ip.response_body,32)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
 }
 
-resource "aws_instance" "instance" {
-    ami = "ami-0ec7f9846da6b0f61"
-    instance_type = "t2.micro"
-    key_name      = aws_key_pair.kp.key_name
-    vpc_security_group_ids = [aws_security_group.sg.id]
+data "aws_ami" "ubuntu_secondary" {
+  provider = aws.secondary
+  most_recent = true
 
-   user_data = <<-EOF
-                #!/bin/bash
-                apt-get update
-                apt-get install -y nginx
-                systemctl start nginx
-                systemctl enable nginx
-                echo "Checking for nginx health"
-                until curl --silent --fail http://localhost:80; do
-                    echo "Waiting for nginx to be up and healthy"
-                    sleep 1
-                done
-                echo "Nginx is up and healthy"
-                EOF
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
 }
 
-resource "aws_key_pair" "kp" {
-  key_name   = "my-key-pair"
-  public_key = file("terraform.pub")
+resource "aws_instance" "instance_1" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
 }
 
-resource "aws_security_group" "sg" {
-    name = "app_sg"
-    
-    ingress {
-        description = "ssh"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [local.my_ip]
-    }
-
-    ingress {
-        description = "ssh"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        description = "http"
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+resource "aws_instance" "instance_2" {
+  
+  depends_on             = [aws_instance.instance_1]
+  ami                    = data.aws_ami.ubuntu_secondary.id
+  instance_type          = "t2.micro"
+  provider = aws.secondary
 }
 
-output "ssh_connection" {
-  value =  format("ssh -i ~/.ssh/terraform ubuntu@%s",aws_instance.instance.public_ip)
-}
-
-output "http_connection" {
-  value =  format("http://%s",aws_instance.instance.public_ip)
-}
 
